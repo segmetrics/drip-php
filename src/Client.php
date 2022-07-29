8d29747c154b37822a2939fb906f5e6402ce9087
Keith Perhac <keith@delfi-net.com>
2022-07-29 07:05:03 -0700

    Merging Upstream

--- src/Client.php
+++ src/Client.php
@@ -4,21 +4,24 @@ namespace Drip;
 
 use Drip\Exception\DripException;
 use Drip\Exception\InvalidArgumentException;
-use Drip\Exception\InvalidApiTokenException;
+use Drip\Exception\InvalidApiKeyException;
+use Drip\Exception\InvalidAccessTokenException;
 use Drip\Exception\InvalidAccountIdException;
 use Drip\Exception\UnexpectedHttpVerbException;
-use GuzzleHttp\Psr7\Request;
 
 /**
  * Drip API
+ *
  * @author Svetoslav Marinov (SLAVI)
  */
 class Client
 {
-    const VERSION = '1.2.0';
+    const VERSION = '1.4.2';
 
     /** @var string */
-    protected $api_token = '';
+    protected $api_key = '';
+    /** @var string */
+    protected $access_token = '';
     /** @var string */
     protected $account_id = '';
     /** @var string */
@@ -28,7 +31,7 @@ class Client
     /** @var integer */
     protected $connect_timeout = 30;
 
-    /** @var callable */
+    /** @var callable|null */
     protected $guzzle_stack_constructor;
 
     const GET    = "GET";
@@ -37,60 +40,141 @@ class Client
     const PUT    = "PUT";
 
     /**
-     * Accepts the token and saves it internally.
+     * Accepts API key or access token and saves it internally.
      *
-     * @param string $api_token e.g. qsor48ughrjufyu2dadraasfa1212424
-     * @param string $account_id e.g. 123456
-     * @param array  $options
+     * @param array|string ...$params
+     *               * `api_key` e.g. "qsor48ughrjufyu2dadraasfa1212424"
+     *               * `access_token` e.g. "daar48ughrjufyu2dadraasfa421121"
+     *               * `account_id` e.g. "123456"
      *               * `api_end_point` (mostly for Drip internal testing)
      *               * `guzzle_stack_constructor` (for test suite, may break at any time, do not use)
-     * @throws Exception
+     * @throws \Exception
      */
-    public function __construct($api_token, $account_id, $options = [])
+    public function __construct(...$params)
     {
-        if (\array_key_exists('api_end_point', $options)) {
-            $this->api_end_point = $options['api_end_point'];
-        }
-        // NOTE: For testing. Could break at any time, please do not depend on this.
-        if (\array_key_exists('guzzle_stack_constructor', $options)) {
-            $this->guzzle_stack_constructor = $options['guzzle_stack_constructor'];
-        }
-        // TODO: allow setting timeouts
+        // Deprecated constructor call.
+        if (is_string($params[0])) {
+            $this->deprecated_constructor(
+                $params[0], // api_key
+                $params[1], // account_id
+                isset($params[2]) ? $params[2] : [] // options
+            );
 
-        $api_token = trim($api_token);
-        if (empty($api_token) || !preg_match('#^[\w-]+$#si', $api_token)) {
-            throw new InvalidApiTokenException("Missing or invalid Drip API token.");
+            return;
         }
-        $this->api_token = $api_token;
 
 
+        if (array_key_exists('access_token', $params[0])) {
+            $this->bearer_auth_setup($params[0]['access_token']);
+        } else if (array_key_exists('api_key', $params[0])) {
+            $this->basic_auth_setup($params[0]['api_key']);
+        } else {
+            throw new InvalidArgumentException("Missing Drip API key or access token.");
+        }
+
+        $this->set_test_options($params[0]);
+        $this->set_account_id($params[0]['account_id']);
+    }
+
+    /**
+     * Accepts API key and stores it internally -- format to be deprecated.
+     *
+     * @param string $api_key
+     * @param string $account_id
+     * @param array{api_end_point?:string, guzzle_stack_constructor?:callable} $options
+     *               * `api_end_point` (for test suite)
+     *               * `guzzle_stack_constructor` (for test suite)
+     * @throws \Exception
+     */
+    protected function deprecated_constructor($api_key, $account_id, $options = [])
+    {
+        $this->basic_auth_setup($api_key);
+        $this->set_account_id($account_id);
+        $this->set_test_options($options);
+    }
+
+    /**
+     * @param string $api_key
+     * @throws \Exception
+     */
+    protected function basic_auth_setup($api_key)
+    {
+        $api_key = trim($api_key);
+        if (empty($api_key) || !preg_match('#^[\w-]+$#si', $api_key)) {
+            throw new InvalidApiKeyException("Missing or invalid Drip API key.");
+        }
+
+        $this->api_key = $api_key;
+    }
+
+    /**
+     * @param string $access_token
+     * @throws \Exception
+     */
+    protected function bearer_auth_setup($access_token)
+    {
+        $access_token = trim($access_token);
+        if (empty($access_token) || !preg_match('#^[\w-]+$#si', $access_token)) {
+            throw new InvalidAccessTokenException("Missing or invalid Drip access token.");
+        }
+
+        $this->access_token = $access_token;
+    }
+
+    /**
+     * @param string $account_id
+     * @throws \Exception
+     */
+    protected function set_account_id($account_id)
+    {
         $account_id = trim($account_id);
         if (empty($account_id) || !preg_match('#^[\w-]+$#si', $account_id)) {
-            throw new InvalidAccountIdException("Missing or invalid Drip API token.");
+            throw new InvalidAccountIdException("Missing or invalid Drip account ID.");
         }
+
         $this->account_id = $account_id;
     }
 
     /**
+     * @param array{api_end_point?:string, guzzle_stack_constructor?:callable} $options
+     *              * `api_end_point`
+     *              * `guzzle_stack_constructor`
+     */
+    protected function set_test_options($options)
+    {
+        if (array_key_exists('api_end_point', $options)) {
+            $this->api_end_point = $options['api_end_point'];
+        }
+
+        // NOTE: For testing. Could break at any time, please do not depend on this.
+        if (array_key_exists('guzzle_stack_constructor', $options)) {
+            $this->guzzle_stack_constructor = $options['guzzle_stack_constructor'];
+        }
+
+        // TODO: allow setting timeouts
+    }
+
+    /**
      * Requests the campaigns for the given account.
-     * @param array $params     Set of arguments
+     *
+     * @param array{status?:string} $params Set of arguments
      *                          - status (optional)
      * @return \Drip\ResponseInterface
+     * @throw \Drip\Exception\InvalidArgumentException
      */
     public function get_campaigns($params)
     {
-        if (isset($params['status'])) {
-            if (!in_array($params['status'], array('active', 'draft', 'paused', 'all'))) {
-                throw new InvalidArgumentException("Invalid campaign status.");
-            }
+        if (isset($params['status']) && !in_array($params['status'], ['active', 'draft', 'paused', 'all'])) {
+            throw new InvalidArgumentException("Invalid campaign status.");
         }
 
-        return $this->make_request("$this->account_id/campaigns", $params);
+        return $this->make_request("{$this->account_id}/campaigns", $params);
     }
 
     /**
      * Fetch a campaign for the given account based on it's ID.
-     * @param array $params     Set of arguments
+     *
+     * @param array{campaign_id?:string} $params Set of arguments
      *                          - campaign_id (required)
      * @return \Drip\ResponseInterface
      */
@@ -103,13 +187,13 @@ class Client
         $campaign_id = $params['campaign_id'];
         unset($params['campaign_id']); // clear it from the params
 
-        return $this->make_request("$this->account_id/campaigns/$campaign_id", $params);
+        return $this->make_request("{$this->account_id}/campaigns/{$campaign_id}", $params);
     }
 
     /**
      * Requests the accounts for the given account.
      * Parses the response JSON and returns an array which contains: id, name, created_at etc
-     * @param void
+     *
      * @return \Drip\ResponseInterface
      */
     public function get_accounts()
@@ -118,31 +202,31 @@ class Client
     }
 
     /**
-     * Sends a request to add a subscriber and returns its record or false
+     * Sends a request to add a subscriber and returns its record or false.
      *
-     * @param array $params
+     * @param array<mixed> $params
      * @return \Drip\ResponseInterface
      */
     public function create_or_update_subscriber($params)
     {
         // The API wants the params to be JSON encoded
         return $this->make_request(
-            "$this->account_id/subscribers",
-            array('subscribers' => array($params)),
+            "{$this->account_id}/subscribers",
+            ['subscribers' => [$params]],
             self::POST
         );
     }
 
     /**
-     * Sends a request to add/update a batch (up to 1000) of subscribers
+     * Sends a request to add/update a batch (up to 1000) of subscribers.
      *
-     * @param array $params
+     * @param array<mixed> $params
      * @return \Drip\ResponseInterface
      */
     public function create_or_update_subscribers($params)
     {
         return $this->make_request(
-            "$this->account_id/subscribers/batches",
+            "{$this->account_id}/subscribers/batches",
             $params,
             self::POST
         );
@@ -151,8 +235,9 @@ class Client
     /**
      * Returns info regarding a particular subscriber
      *
-     * @param array $params
+     * @param array<mixed> $params
      * @return \Drip\ResponseInterface
+     * @throw \Drip\Exception\InvalidArgumentException
      */
     public function fetch_subscriber($params)
     {
@@ -168,7 +253,31 @@ class Client
 
         $subscriber_id = urlencode($subscriber_id);
 
-        return $this->make_request("$this->account_id/subscribers/$subscriber_id");
+        return $this->make_request("{$this->account_id}/subscribers/{$subscriber_id}");
+    }
+
+    /**
+     * Returns info regarding a particular subscriber subscriptions to campaigns.
+     *
+     * @param array<mixed> $params
+     * @return \Drip\ResponseInterface
+     * @throw \Drip\Exception\InvalidArgumentException
+     */
+    public function fetch_subscriber_campaigns($params)
+    {
+        if (!empty($params['subscriber_id'])) {
+            $subscriber_id = $params['subscriber_id'];
+            unset($params['subscriber_id']); // clear it from the params
+        } else if (!empty($params['email'])) {
+            $subscriber_id = $params['email'];
+            unset($params['email']); // clear it from the params
+        } else {
+            throw new InvalidArgumentException("Subscriber ID or Email was not specified. You must specify either Subscriber ID or Email.");
+        }
+
+        $subscriber_id = urlencode($subscriber_id);
+
+        return $this->make_request("{$this->account_id}/subscribers/{$subscriber_id}/campaign_subscriptions");
     }
 
     /**
@@ -178,20 +287,21 @@ class Client
      */
     public function fetch_subscribers($params)
     {
-        return $this->make_request("$this->account_id/subscribers", $params);
+        return $this->make_request("{$this->account_id}/subscribers", $params);
     }
 
     /**
      * Subscribes a user to a given campaign for a given account.
      *
-     * @param array $params
+     * @param array<mixed> $params
+     * @return \Drip\ResponseInterface
+     * @throw \Drip\Exception\InvalidArgumentException
      */
     public function subscribe_subscriber($params)
     {
         if (empty($params['campaign_id'])) {
             throw new InvalidArgumentException("Campaign ID not specified");
         }
-
         $campaign_id = $params['campaign_id'];
         unset($params['campaign_id']); // clear it from the params
 
@@ -203,17 +313,22 @@ class Client
             $params['double_optin'] = true;
         }
 
-        // The API wants the params to be JSON encoded
-        $req_params = array('subscribers' => array($params));
+        // The API wants the params to be JSON encoded.
+        $req_params = ['subscribers' => [$params]];
 
-        return $this->make_request("$this->account_id/campaigns/$campaign_id/subscribers", $req_params, self::POST);
+        return $this->make_request(
+            "{$this->account_id}/campaigns/{$campaign_id}/subscribers",
+            $req_params,
+            self::POST
+        );
     }
 
     /**
-     *
      * Some keys are removed from the params so they don't get send with the other data to Drip.
      *
-     * @param array $params
+     * @param array<mixed> $params
+     * @return \Drip\ResponseInterface
+     * @throw \Drip\Exception\InvalidArgumentException
      */
     public function unsubscribe_subscriber($params)
     {
@@ -228,7 +343,11 @@ class Client
         }
 
         $subscriber_id = urlencode($subscriber_id);
-        return $this->make_request("$this->account_id/subscribers/$subscriber_id/unsubscribe", $params, self::POST);
+        return $this->make_request(
+            "{$this->account_id}/subscribers/{$subscriber_id}/unsubscribe",
+            $params,
+            self::POST
+        );
     }
 
     /**
@@ -242,11 +361,34 @@ class Client
     }
 
     /**
+     * This calls DELETE /:account_id/subscribers/:id_or_email to delete a subscriber.
      *
+     * @param array<mixed> $params
+     * @return \Drip\ResponseInterface
+     * @throw \Drip\Exception\InvalidArgumentException
+     */
+    public function delete_subscriber($params)
+    {
+        if (!empty($params['subscriber_id'])) {
+            $subscriber_id = $params['subscriber_id'];
+            unset($params['subscriber_id']); // clear it from the params
+        } else if (!empty($params['email'])) {
+            $subscriber_id = $params['email'];
+            unset($params['email']); // clear it from the params
+        } else {
+            throw new InvalidArgumentException("Subscriber ID or Email was not specified. You must specify either Subscriber ID or Email.");
+        }
+
+        $subscriber_id = urlencode($subscriber_id);
+        return $this->make_request("{$this->account_id}/subscribers/{$subscriber_id}", $params, self::DELETE);
+    }
+
+    /**
      * This calls POST /:account_id/tags to add the tag. It just returns some status code no content
      *
-     * @param array $params
-     * @param bool $status
+     * @param array<mixed> $params
+     * @return \Drip\ResponseInterface
+     * @throw \Drip\Exception\InvalidArgumentException
      */
     public function tag_subscriber($params)
     {
@@ -259,17 +401,17 @@ class Client
         }
 
         // The API wants the params to be JSON encoded
-        $req_params = array('tags' => array($params));
+        $req_params = ['tags' => [$params]];
 
-        return $this->make_request("$this->account_id/tags", $req_params, self::POST);
+        return $this->make_request("{$this->account_id}/tags", $req_params, self::POST);
     }
 
     /**
-     *
      * This calls DELETE /:account_id/tags to remove the tags. It just returns some status code no content
      *
      * @param array $params
-     * @param bool $status success or failure
+     * @return \Drip\ResponseInterface
+     * @throw \Drip\Exception\InvalidArgumentException
      */
     public function untag_subscriber($params)
     {
@@ -282,17 +424,17 @@ class Client
         }
 
         // The API wants the params to be JSON encoded
-        $req_params = array('tags' => array($params));
+        $req_params = ['tags' => [$params]];
 
-        return $this->make_request("$this->account_id/tags", $req_params, self::DELETE);
+        return $this->make_request("{$this->account_id}/tags", $req_params, self::DELETE);
     }
 
     /**
-     *
      * Posts an event specified by the user.
      *
      * @param array $params
-     * @param bool
+     * @return \Drip\ResponseInterface
+     * @throw \Drip\Exception\InvalidArgumentException
      */
     public function record_event($params)
     {
@@ -301,9 +443,9 @@ class Client
         }
 
         // The API wants the params to be JSON encoded
-        $req_params = array('events' => array($params));
+        $req_params = ['events' => [$params]];
 
-        return $this->make_request("$this->account_id/events", $req_params, self::POST);
+        return $this->make_request("{$this->account_id}/events", $req_params, self::POST);
     }
 
     /**
@@ -361,14 +503,15 @@ class Client
     }
 
     /**
+     * Send a request.
      *
      * @param string $url
-     * @param array $params
-     * @param int $req_method
+     * @param array<mixed> $params
+     * @param string $req_method
      * @return \Drip\ResponseInterface
-     * @throws Exception
+     * @throws \Exception
      */
-    protected function make_request($url, $params = array(), $req_method = self::GET)
+    protected function make_request($url, $params = [], $req_method = self::GET)
     {
         if ($this->guzzle_stack_constructor) {
             // This can be replaced with `($this->guzzle_stack_constructor)()` once we drop PHP5 support.
@@ -379,6 +522,7 @@ class Client
             $stack = \GuzzleHttp\HandlerStack::create();
             // @codeCoverageIgnoreEnd
         }
+
         $client = new \GuzzleHttp\Client([
             'base_uri' => $this->api_end_point,
             'handler' => $stack,
@@ -391,10 +535,15 @@ class Client
                 'User-Agent' => $this->user_agent(),
                 'Accept' => 'application/json, text/javascript, */*; q=0.01',
                 'Content-Type' => 'application/vnd.api+json',
-                'Authorization' => 'Bearer ' . $this->api_token,
             ],
             'http_errors' => true,
         ];
+
+        if (!empty($this->api_key)) {
+            $req_params['auth'] = [$this->api_key, ''];
+        } else if (!empty($this->access_token)) {
+            $req_params['headers']['Authorization'] = 'Bearer ' . $this->access_token;
+        }
 
         switch ($req_method) {
             case self::GET:
@@ -410,7 +559,6 @@ class Client
             default:
                 // @codeCoverageIgnoreStart
                 throw new UnexpectedHttpVerbException("Unexpected HTTP verb $req_method");
-                break;
                 // @codeCoverageIgnoreEnd
         }
 
